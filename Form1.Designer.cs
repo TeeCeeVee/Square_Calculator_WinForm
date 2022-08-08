ba6226e98f34c027b4c4ba89f1df14348ce53f0b
thea Villanueva <tei.villanueva@gmail.com>
2022-08-08 00:21:58 -0700

    Create a calculator that squares a number

---
+++ Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace SquaresCalculatorApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btn_calculate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_lowerLimit = new System.Windows.Forms.TextBox();
+            this.txt_UpperLimit = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_countBy = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(30, 132);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(366, 354);
+            this.listBox1.TabIndex = 0;
+            // 
+            // btn_calculate
+            // 
+            this.btn_calculate.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.btn_calculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_calculate.Location = new System.Drawing.Point(47, 46);
+            this.btn_calculate.Name = "btn_calculate";
+            this.btn_calculate.Size = new System.Drawing.Size(102, 68);
+            this.btn_calculate.TabIndex = 1;
+            this.btn_calculate.Text = "Calculate Squares";
+            this.btn_calculate.UseVisualStyleBackColor = false;
+            this.btn_calculate.Click += new System.EventHandler(this.btn_calculate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(192, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Lower Limit";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(192, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Upper Limit";
+            // 
+            // txt_lowerLimit
+            // 
+            this.txt_lowerLimit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_lowerLimit.Location = new System.Drawing.Point(321, 46);
+            this.txt_lowerLimit.Name = "txt_lowerLimit";
+            this.txt_lowerLimit.Size = new System.Drawing.Size(75, 30);
+            this.txt_lowerLimit.TabIndex = 4;
+            this.txt_lowerLimit.Text = "3";
+            // 
+            // txt_UpperLimit
+            // 
+            this.txt_UpperLimit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_UpperLimit.Location = new System.Drawing.Point(321, 96);
+            this.txt_UpperLimit.Name = "txt_UpperLimit";
+            this.txt_UpperLimit.Size = new System.Drawing.Size(75, 30);
+            this.txt_UpperLimit.TabIndex = 5;
+            this.txt_UpperLimit.Text = "12";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(427, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Count By:";
+            // 
+            // txt_countBy
+            // 
+            this.txt_countBy.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_countBy.Location = new System.Drawing.Point(532, 51);
+            this.txt_countBy.Name = "txt_countBy";
+            this.txt_countBy.Size = new System.Drawing.Size(100, 30);
+            this.txt_countBy.TabIndex = 7;
+            this.txt_countBy.TextChanged += new System.EventHandler(this.txt_countBy_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 524);
+            this.Controls.Add(this.txt_countBy);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_UpperLimit);
+            this.Controls.Add(this.txt_lowerLimit);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_calculate);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Squares Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btn_calculate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_lowerLimit;
+        private System.Windows.Forms.TextBox txt_UpperLimit;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_countBy;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SquaresCalculatorApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_calculate_Click(object sender, EventArgs e)
+        {
+            int lowerLimit = 0;
+            int upperLimit = 10;
+            int countBy = 1;
+
+            listBox1.Items.Clear();
+
+            try
+            {
+                // store values from textbox to variables
+                lowerLimit = int.Parse(txt_lowerLimit.Text);
+                upperLimit = int.Parse(txt_UpperLimit.Text);
+                countBy = int.Parse(txt_countBy.Text); 
+
+                if (lowerLimit > upperLimit)
+                {
+                    // swap numbers
+                    int temp = lowerLimit;
+                    lowerLimit = upperLimit;
+                    upperLimit = temp;
+                }
+
+                for (int i = lowerLimit; i < upperLimit; i += countBy)
+                {
+                    listBox1.Items.Add("i = " + i + " squared = " + i * i);
+                    
+                }
+
+
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Enter numbers only");
+            }
+
+       
+
+
+
+        }
+
+        private void txt_countBy_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
